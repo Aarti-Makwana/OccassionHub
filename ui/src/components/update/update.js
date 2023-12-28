68fd82d71b792e8a6193c55c4403924c2059b20c
dheeraj <dheerajsinghchouhan94@gmail.com>
2023-12-28 14:42:30 +0530

    updated by dheeraj in dheeraj branch 14:42

--- ui/src/components/update/update.js
+++ ui/src/components/update/update.js
@@ -1,6 +1,19 @@
 function update(){
     return(<>
         <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
+        <h1>hugjh</h1>
     </>);
 }
 
