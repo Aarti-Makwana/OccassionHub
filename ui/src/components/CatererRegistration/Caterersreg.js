8949e82993f322b923ff76ed4e014e0302c21c7c
dheeraj <dheerajsinghchouhan94@gmail.com>
2024-01-11 23:27:08 +0530

    updated by dheeraj at 23:26 after add of jayanti code and mongodb atlas

--- ui/src/components/CatererRegistration/Caterersreg.js
+++ ui/src/components/CatererRegistration/Caterersreg.js
@@ -8,17 +8,17 @@ function Caterersreg() {
   const [isCatererManagerRegistrationModal, setCatererRegistrationModal] = useState(false)
   const [caterer, setCatererdata] = useState({});
   const getData = (event) => {
-    let {name,value} = event.target;
-      if(event.target.type=='file'){
-          value = event.target.files[0];
-          setCatererdata({
-            ...caterer,
-            [name]:value
-        });            
-      }else{
-        setCatererdata({
-          ...caterer,
-          [name]:value
+    let { name, value } = event.target;
+    if (event.target.type == 'file') {
+      value = event.target.files[0];
+      setCatererdata({
+        ...caterer,
+        [name]: value
+      });
+    } else {
+      setCatererdata({
+        ...caterer,
+        [name]: value
       });
     }
   };
@@ -28,21 +28,22 @@ function Caterersreg() {
     const userCookieEmail = jscookie.get("user");
     caterer["userEmail"] = userCookieEmail;
     const formData = new FormData();
-    for(var index in caterer){
-      console.log("caterer[index] : ",caterer[index])
-      if(caterer[index]){
-        formData.append(index,caterer[index])
+    for (var index in caterer) {
+      console.log("caterer[index] : ", caterer[index])
+      if (caterer[index]) {
+        formData.append(index, caterer[index])
       }
     }
     var result = RegisterCatererData(formData);
-    console.log("rsult in catrere controller : ",result);
-   result.then((result) => {
-      if(result.status==201){
+    console.log("rsult in catrere controller : ", result);
+    result.then((result) => {
+      if (result.status == 201) {
         alert("registration sucefully...!")
+        setCatererRegistrationModal(false);
       }
-    }).catch((err)=>{
+    }).catch((err) => {
       alert("error in registration......!!");
-      console.log("error in catch.....!!"+err);
+      console.log("error in catch.....!!" + err);
     });
   };
 
@@ -54,56 +55,56 @@ function Caterersreg() {
     <h6 onClick={() => { setCatererRegistrationModal(true) }}>Caterer Manager</h6>
     <form onSubmit={handleSubmitCaterer} className="form-group" enctype="multipart/form-data">
 
-    <Modal size="lg" show={isCatererManagerRegistrationModal} onHide={handleCatererManagerModalClose} centered  >
-      <Modal.Body className='p-0'>
-        <div className="container bg-black">
-          <div className="row d-flex justify-content-center align-items-center h-50" >
-            <div className='col-lg-6'>
-              <img src={caterer1} width={'100%'} alt="Sample photo" />
-            </div>
-            <div className="col-lg-6 py-4">
-              <h2 className="text-center text-white">Caterer Manager Form</h2>
-              <form className="px-md-2">
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <input type="text" name="Businessname" onChange={getData} value={caterer.Businessname} className="form-control input-field" placeholder="Enter your Business name" />
+      <Modal size="lg" show={isCatererManagerRegistrationModal} onHide={handleCatererManagerModalClose} centered  >
+        <Modal.Body className='p-0'>
+          <div className="container bg-black">
+            <div className="row d-flex justify-content-center align-items-center h-50" >
+              <div className='col-lg-6'>
+                <img src={caterer1} width={'100%'} alt="Sample photo" />
+              </div>
+              <div className="col-lg-6 py-4">
+                <h2 className="text-center text-white">Caterer Manager Form</h2>
+                <form className="px-md-2">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <input type="text" name="Businessname" onChange={getData} value={caterer.Businessname} className="form-control input-field" placeholder="Enter your Business name" />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <input type="text" name="Specialization" onChange={getData} value={caterer.Specialization} id="name2" className="form-control input-field" placeholder="Enter your Specialization" />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <select className="w-100 mt-4" id="select1" name="FoodType" onChange={getData} value={caterer.FoodType} aria-label="Default select example">
+                        <option selected>Select Food Type</option>
+                        <option value="Vegetarian">Vegetarian</option>
+                        <option value="Non-Vegetarian">Non-Vegetarian</option>
+                      </select>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <input type="text" onChange={getData} value={caterer.ServiceCharges} id="name2" className="form-control input-field" name="ServiceCharges" placeholder="Enter your Service Charges" />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <label for="formFile" id="addgallery" className="form-label">Add Your Gallery</label>
+                        <input onChange={getData} type="file" id="docs" name="docs" />
+                      </div>
                     </div>
                   </div>
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <input type="text" name="Specialization" onChange={getData} value={caterer.Specialization} id="name2" className="form-control input-field" placeholder="Enter your Specialization" />
-                    </div>
+                  <div className="d-flex justify-content-center mt-3">
+                    <button type="submit" className="ourbtn w-50">Submit</button>
                   </div>
-                  <div className="col-md-12">
-                    <select className="w-100 mt-4" id="select1" name="FoodType" onChange={getData} value={caterer.FoodType} aria-label="Default select example">
-                      <option selected>Select Food Type</option>
-                      <option value="Vegetarian">Vegetarian</option>
-                      <option value="Non-Vegetarian">Non-Vegetarian</option>
-                    </select>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <input type="text" onChange={getData} value={caterer.ServiceCharges} id="name2" className="form-control input-field" name="ServiceCharges" placeholder="Enter your Service Charges" />
-                    </div>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <label for="formFile" id="addgallery" className="form-label">Add Your Gallery</label>
-                      <input  onChange={getData}  type="file" id="docs" name="docs"/>
-                    </div>
-                  </div>
-                </div>
-                <div className="d-flex justify-content-center mt-3">
-                  <button type="submit" className="ourbtn w-50">Submit</button>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </Modal.Body>
-    </Modal>
-   </form>
+        </Modal.Body>
+      </Modal>
+    </form>
   </>
   );
 }
--- ui/src/components/VenueRegistration/Venueform.js
+++ ui/src/components/VenueRegistration/Venueform.js
@@ -8,17 +8,17 @@ function Venue() {
   const [isVenueManagerRegistrationModal, setVenueRegistrationModal] = useState(false);
   const [venueData, setVenuedata] = useState({});
   const getData = (event) => {
-    let {name,value} = event.target;
-      if(event.target.type=='file'){
-          value = event.target.files[0];
-          setVenuedata({
-            ...venueData,
-            [name]:value
-        });            
-      }else{
-        setVenuedata({
-          ...venueData,
-          [name]:value
+    let { name, value } = event.target;
+    if (event.target.type == 'file') {
+      value = event.target.files[0];
+      setVenuedata({
+        ...venueData,
+        [name]: value
+      });
+    } else {
+      setVenuedata({
+        ...venueData,
+        [name]: value
       });
     }
   };
@@ -29,20 +29,21 @@ function Venue() {
     const userCookieEmail = jscookie.get("user");
     venueData["venueEmail"] = userCookieEmail;
     const formData = new FormData();
-    for(var index in venueData){
-      if(venueData[index]){
-        formData.append(index,venueData[index])
+    for (var index in venueData) {
+      if (venueData[index]) {
+        formData.append(index, venueData[index])
       }
     }
     var result = RegistervenueData(formData);
-    console.log("rsult in venueData controller : ",result);
-   result.then((result) => {
-      if(result.status==201){
-        alert("registration sucefully...!!")
+    console.log("rsult in venueData controller : ", result);
+    result.then((result) => {
+      if (result.status == 201) {
+        alert("registration sucefully...!!");
+        setVenueRegistrationModal(false);
       }
-    }).catch((err)=>{
+    }).catch((err) => {
       alert("error in registration......!!");
-      console.log("error in catch.....!!"+err);
+      console.log("error in catch.....!!" + err);
     });
   };
 
@@ -54,56 +55,56 @@ function Venue() {
     <h6 onClick={() => { setVenueRegistrationModal(true) }}>Venue Manager</h6>
     <form onSubmit={handleSubmitVenue} className="form-group" enctype="multipart/form-data">
 
-    <Modal size="lg" show={isVenueManagerRegistrationModal} onHide={handleVenueManagerModalClose} centered  >
-      <Modal.Body className='p-0'>
-        <div className="container bg-black">
-          <div className="row d-flex justify-content-center align-items-center h-50" >
-            <div className='col-lg-6'>
-              <img src={caterer} width={'100%'} alt="Sample photo" />
-            </div>
-            <div className="col-lg-6 py-4">
-              <h2 className="text-center text-white">Venue Manager Form</h2>
-              <form className="px-md-2">
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <input type="text" id="name1" className="form-control input-field" placeholder="Enter your Venue name" onChange={getData} name='VenueName' />
+      <Modal size="lg" show={isVenueManagerRegistrationModal} onHide={handleVenueManagerModalClose} centered  >
+        <Modal.Body className='p-0'>
+          <div className="container bg-black">
+            <div className="row d-flex justify-content-center align-items-center h-50" >
+              <div className='col-lg-6'>
+                <img src={caterer} width={'100%'} alt="Sample photo" />
+              </div>
+              <div className="col-lg-6 py-4">
+                <h2 className="text-center text-white">Venue Manager Form</h2>
+                <form className="px-md-2">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <input type="text" id="name1" className="form-control input-field" placeholder="Enter your Venue name" onChange={getData} name='VenueName' />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <input type="text" id="name2" className="form-control input-field" placeholder="Enter your venue location" onChange={getData} name='venueLocation' />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <input type="text" id="name2" className="form-control input-field" placeholder="Enter your Venue Price/per Day" onChange={getData} name='venuePrice' />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="mt-4">
+                        <label for="formFile" id="addgallery" className="form-label">Add Your Gallery</label>
+                        <input onChange={getData} className="form-control" type="file" id="docs" name='docs' />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <select onChange={getData} name='VenueType' class="w-100 mt-4" id="select1" aria-label="Default select example">
+                        <option selected>Select Venue type</option>
+                        <option value="garden">All</option>
+                        <option value="2">Garden+Hall</option>
+                        <option value="3">Garden+Room+Hall</option>
+                      </select>
                     </div>
                   </div>
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <input type="text" id="name2" className="form-control input-field" placeholder="Enter your venue location" onChange={getData} name='venueLocation'/>
-                    </div>
+                  <div className="d-flex justify-content-center mt-3">
+                    <button type="submit" className="ourbtn w-50">Submit</button>
                   </div>
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <input type="text" id="name2" className="form-control input-field" placeholder="Enter your Venue Price/per Day" onChange={getData} name='venuePrice'/>
-                    </div>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="mt-4">
-                      <label for="formFile" id="addgallery" className="form-label">Add Your Gallery</label>
-                      <input onChange={getData} className="form-control" type="file" id="docs" name='docs'/>
-                    </div>
-                  </div>
-                  <div className="col-md-12">
-                    <select onChange={getData} name='VenueType' class="w-100 mt-4" id="select1" aria-label="Default select example">
-                      <option selected>Select Venue type</option>
-                      <option value="garden">All</option>
-                      <option value="2">Garden+Hall</option>
-                      <option value="3">Garden+Room+Hall</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="d-flex justify-content-center mt-3">
-                  <button type="submit" className="ourbtn w-50">Submit</button>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </Modal.Body>
-    </Modal>
+        </Modal.Body>
+      </Modal>
     </form>
   </>)
 }
